3a1ef03421d59bfd78354aac034ef561b3d3f596
ifeisaucy <102739361+ifeisaucy@users.noreply.github.com>
2022-05-24 14:16:36 +0400

    Add files via upload

---
+++ function.js
@@ -0,0 +1,22 @@
+//QUESTION:
+var musicians = 20;
+if (musicians === 1) {
+    console.log("solo");
+}// your code goes here
+else if (musicians === 2) {
+    console.log("duet");
+}// your code goes here
+else if (musicians === 3) {
+    console.log("trio");
+}// your code goes here
+else if (musicians === 4) {
+    console.log("quartet");
+}// your code goes here
+else if (musicians > 4) {
+    console.log("this is a large group");
+}// your code goes here
+
+else {
+    console.log("not a group");
+}
+//THIS CODE WAS CONTRIBUTED BY BABARIMISA IFEOLUWA
